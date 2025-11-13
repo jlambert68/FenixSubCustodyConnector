3d11efb88cfa5ab0f47e424cd0e387b86eaef46a
jlambert68 <jonas.lambert@drakon.se>
2025-11-13 12:42:17 +0100

    Reverted to original SubInstruction-structure

--- embeddedSubInstructions.go
+++ embeddedSubInstructions.go
@@ -4,10 +4,10 @@ import (
 	_ "embed"
 )
 
-//go:embed supportedSubInstructions/supportedSubInstructions_All.json
+//go:embed supportedSubInstructions/supportedSubInstructions_All-NEXT.json
 var embeddedFile_SupportedSubInstructions []byte
 
-//go:embed supportedSubInstructions/supportedSubInstructions_VerifyMQTypeMT_VerifyContentTypeMT5xx.json
+//go:embed supportedSubInstructions/supportedSubInstructions_VerifyMQTypeMT_VerifyContentTypeMT5xx-NEXT.json
 var embeddedFiles_SupportedSubInstructions_VerifyMQTypeMT_VerifyContentTypeMT5xx []byte
 
 var embeddedFiles_SupportedSubInstructionsPerTestInstructionSlice = [][]byte{
